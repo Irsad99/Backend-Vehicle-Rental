857ca54855618e6a419d353d0bb7b167e84de796
Irsad99 <irsadmoh01@gmail.com>
2022-06-07 11:35:25 +0700

    Info : update validate Email type(string) to email

--- src/modules/v1/users/user_model.go
+++ src/modules/v1/users/user_model.go
@@ -8,7 +8,7 @@ type User struct {
 	User_ID   uint      `gorm:"primaryKey" json:"user_id"`
 	Name      string    `json:"name" valid:"type(string), required"`
 	Gender    string    `json:"gender" valid:"type(string), required"`
-	Email     *string   `json:"email" valid:"type(string), required"`
+	Email     *string   `json:"email" valid:"email, required"`
 	Phone     string    `json:"phone" valid:"type(string), required"`
 	Birth     string    `json:"birth" valid:"type(string), required"`
 	Address   string    `json:"address" valid:"type(string), required"`
