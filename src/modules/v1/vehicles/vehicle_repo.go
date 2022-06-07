b4651a7190e1675a5f8c4d238b04933d0ac8ebfb
Irsad99 <irsadmoh01@gmail.com>
2022-06-07 11:39:39 +0700

    Info : Update variable vehicles global to lacal and update handler

--- src/modules/v1/vehicles/vehicle_repo.go
+++ src/modules/v1/vehicles/vehicle_repo.go
@@ -7,7 +7,7 @@ import (
 	"github.com/asaskevich/govalidator"
 )
 
-var vehicles Vehicles
+// var vehicles Vehicles
 var response helpers.Response
 
 type vehicle_repo struct {
@@ -19,6 +19,8 @@ func NewRepo(grm *gorm.DB) *vehicle_repo {
 }
 
 func (r *vehicle_repo) FindAll() (*helpers.Response, error) {
+
+	var vehicles Vehicles
 
 	result := r.db.Order("vehicle_id desc").Find(&vehicles)
 
@@ -32,6 +34,8 @@ func (r *vehicle_repo) FindAll() (*helpers.Response, error) {
 }
 
 func (r *vehicle_repo) Search(category ...interface{}) (*helpers.Response, error) {
+
+	var vehicles Vehicles
 
 	result := r.db.Where("category = ? AND location = ? ", category[0], category[1]).Find(&vehicles)
 
@@ -51,6 +55,8 @@ func (r *vehicle_repo) Search(category ...interface{}) (*helpers.Response, error
 
 func (r *vehicle_repo) SortByPrice(price *int) (*helpers.Response, error) {
 
+	var vehicles Vehicles
+
 	result := r.db.Order("price desc").Where("CAST(price AS int) > ?", price).Find(&vehicles)
 
 	if result.RowsAffected < 1 {
@@ -69,6 +75,8 @@ func (r *vehicle_repo) SortByPrice(price *int) (*helpers.Response, error) {
 
 func (r *vehicle_repo) Popular(rating *int) (*helpers.Response, error) {
 
+	var vehicles Vehicles
+
 	result := r.db.Where("rating >= ?", rating).Order("rating desc").Find(&vehicles)
 
 	if result.RowsAffected < 1 {
@@ -86,6 +94,8 @@ func (r *vehicle_repo) Popular(rating *int) (*helpers.Response, error) {
 }
 
 func (r *vehicle_repo) Add(data *Vehicle) (*helpers.Response, error) {
+
+	var vehicles Vehicles
 
 	_, err := govalidator.ValidateStruct(data)
 	if err != nil {
@@ -113,6 +123,8 @@ func (r *vehicle_repo) Add(data *Vehicle) (*helpers.Response, error) {
 
 func (r *vehicle_repo) Delete(data *int) (*helpers.Response, error) {
 
+	var vehicles Vehicles
+
 	getdata := r.db.First(&vehicles, &data)
 	if getdata.RowsAffected < 1 {
 		res := response.ResponseJSON(404, vehicles)
@@ -132,7 +144,9 @@ func (r *vehicle_repo) Delete(data *int) (*helpers.Response, error) {
 
 func (r *vehicle_repo) Update(id *int, data *Vehicle) (*helpers.Response, error) {
 
-	result := r.db.Model(&Vehicle{}).Where("vehicle_id = ?", &id).Updates(&Vehicle{Price: data.Price, Status: data.Status})
+	var vehicles Vehicles
+
+	result := r.db.Model(&Vehicle{}).Where("vehicle_id = ?", &id).Updates(&Vehicle{Name: data.Name, Location: data.Location, Description: data.Description, Price: data.Price, Status: data.Status, Stock: data.Stock, Category: data.Category, Image: data.Image, Rating: data.Rating})
 
 	if result.Error != nil {
 		res := response.ResponseJSON(400, vehicles)
