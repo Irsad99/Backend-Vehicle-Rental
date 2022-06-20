1307b3c5d1c89d22926ab90a82d87c37693704de
Irsad99 <irsadmoh01@gmail.com>
2022-06-20 18:00:13 +0700

    bismillah deploy

--- src/configs/serve/server.go
+++ src/configs/serve/server.go
@@ -37,10 +37,10 @@ func serve(cmd *cobra.Command, args []string) error {
 	// mainRoute.Use(mainRoute.W)
 
 	if mainRoute, err := routers.New(); err == nil {
-		var addrs string = ""
+		var addrs string = "127.0.0.1:8080"
 
 		if pr := os.Getenv("PORT"); pr != "" {
-			addrs = "127.0.0.1:" + pr
+			addrs = ":" + pr
 		}
 
 		log.Println("App running on " + addrs)
